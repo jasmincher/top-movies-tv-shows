1ef1364f59a58e9f812e95433f582d34b3aa9fe4
Jas <c.jasminher@gmail.com>
2019-07-10 15:42:46 -0400

    Added react router and folder structure

---
+++ src/components/Routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Route} from 'react-router';
+
+
+
+const Routes = () => {
+    return(
+        <div>
+        <Route exact path="/" component={} />
+        <Route path="" component={}/>
+        <Route path="" component={}/>
+        <Route path="" component={}/>
+    </div>
+        )
+}
+
+export default Routes;
