34a32dbdf9d638bb2c23c050374d45c81aa12a59
Jas <c.jasminher@gmail.com>
2019-07-25 21:11:39 -0400

    Made nav items align to the left when navbar collapsed

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,8 +11,8 @@ function Card(props) {
             <div className="card-container" >
 
                 <div className="card-overlay">
-                    <p className="title">{props.image.title} {props.image.name}</p>
-                    <p className="overview">{props.image.overview} </p>
+                    <p className="card-title">{props.image.title} {props.image.name}</p>
+                    <p className="card-overview">{props.image.overview} </p>
                 </div>
                 <img src={`https://image.tmdb.org/t/p/original/${props.image.poster_path}`} className="card-size" />
 
