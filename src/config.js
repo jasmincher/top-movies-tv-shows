454fd7ab3f242397bf9ebae463e2f6b16cefebdf
Jas <c.jasminher@gmail.com>
2019-07-30 10:51:41 -0400

    .

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const config = {
+
+    API_KEY: '1fb718e33990d4d733d6e019892804af',
+    SESSION_ID: 'ff716a659cc0ae54f11d8d6e40935f4d21fd9a60',
+    ACCOUNT_ID: 'jasmincher'
+    
+}
+
+export default config;
