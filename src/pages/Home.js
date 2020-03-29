eaccfdc03c62270bb2e49c878156d562364a64e8
Jas <c.jasminher@gmail.com>
2020-03-28 20:35:58 -0400

    style: Changed content for home page, added gif and different text, added style to gif

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import '../css/Home.css'
-
+import React from "react";
+import "../css/Home.css";
 
 function Home() {
   return (
     <div id="home-container">
-      <h2>Why Must Watch TV Shows and Movies?</h2>
+      <h2 style={{ textAlign: "center", marginBottom: "20px" }}>
+        Tired of searching? Me too.
+      </h2>
       <p>
-    I'm very picky when it comes to movies and tv shows. After hours, days, and even weeks of looking at movie and tv show trailers, I got bored of seeing the typical plot, which I know will lead to the typical ending. I like watching tv shows and movies that make me feel more than just sad, happy, or scared. I like to watch things that make me think and question. I also got bored of browsing websites that only displayed the movie poster with a long summary, and no trailer. That's why I created this website. Simple. To the point. And movies and tv shows you might have never heard of. Most of the tv shows and movies included in these lists are science fiction, so if you like this genre, you defintely have to checkout some of these titles. However, I do have in these lists other genres as well. These titles are definetely worth watching, as many of them are underrated, and are overshadowed by mainstream titles. Although I'm one to avoid tv shows that have been cancelled, I did include some tv shows that have been cancelled. I included these tv shows because they are worth watching, and they left me wondering of what could have happened.
+        I know what it feels like to waste hours in searching for something good to watch, and finally finding something interesting, but now you're too sleepy to watch. That's why I created this website, so anyone can discover great shows and movies, quick and easy. All of these movies and
+        shows are worth watching, even if it's not in your favorite genre selection. 
+      </p>
 
-    </p>
-
-    
+      <img
+        src="https://media.giphy.com/media/3o6gDWOo1d6zKmQCru/source.gif"
+        style={{ width: "100%", marginTop: "20px" }}
+      />
     </div>
-
   );
 }
 
