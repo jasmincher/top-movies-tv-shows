3bce606d1fded6bf5aebe12a3c1415fc8a3e7075
Jas <c.jasminher@gmail.com>
2020-04-06 18:11:15 -0400

    feat: Added new object to contain missing posters and their corresponding title

---
+++ src/components/MissingPosters.js
@@ -0,0 +1,12 @@
+import drown from "../assets/images/Drown.jpg";
+import parallels from "../assets/images/Parallels.jpg";
+import mamitas from "../assets/images/Mamitas.jpg";
+
+export const missing = [
+    
+  { posterImg: drown, title: "Don't Let Me Drown"},
+
+  { posterImg: parallels, title: "Parallels" },
+
+  { posterImg: mamitas, title: "Mamitas" },
+];
